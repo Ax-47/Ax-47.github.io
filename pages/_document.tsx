2f1f9873684e7c50f7791a69cb5fa358c9115a20
Ax-47 <armingLwnza@gmail.com>
2022-12-19 09:00:35 +0700

    แปป

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="th">
+       <link href='https://fonts.googleapis.com/css?family=JetBrains Mono' rel='stylesheet'/>
       <Head />
       <body>
         <Main />
